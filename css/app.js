78d99999d7864c9f2bd295aa582f3226d8c2d344
usep.gunawan <usep.gunawan@pgncom.co.id>
2024-08-04 18:07:24 +0700

    update

--- css/app.js
+++ css/app.js
@@ -156,4 +156,31 @@ toggleClose.addEventListener('click', handleClick);
         }
     });
     
+
+    // Get references to elements
+    const gallery = select('.gallery');
+    const lightbox = select('#lightbox');
+    const lightboxImage = select('#lightbox-image');
+    const closeButton = select('#close');
+
+    // Add event listener to each image
+    gallery.addEventListener('click', e => {
+        if (e.target.classList.contains('gallery-image')) {
+        const imageSrc = e.target.src;
+        lightboxImage.src = imageSrc;
+        lightbox.style.display = 'flex';
+        }
+    });
+
+    // Close lightbox when close button is clicked
+    closeButton.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+    });
+
+    // Close lightbox when clicking outside the image
+    lightbox.addEventListener('click', e => {
+        if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+        }
+    });
 })();
